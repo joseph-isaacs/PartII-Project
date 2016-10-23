ad6ea165d5634d3fc90bcffa123e0675d975e4a6
Curdled <joe.isaacs@live.co.uk>
2016-10-23 19:38:01 +0100

    adeed a conid and varid test

---
+++ JVHC/JVHC/test/TestConidAndVaridLexer.hs
@@ -0,0 +1,14 @@
+module TestConidAndVaridLexer(tests)
+  where
+
+import Lexer
+import AST
+import Test.HUnit
+
+test1 = "Conid" ~: alexScanTokens "Hell1o"  ~=? [Conid "Hell1o"]
+test2 = "Varid" ~: alexScanTokens "hel1r3"      ~=? [Varid "hel1r3"]
+test3 = "ReservedID"       ~: alexScanTokens "case Hello"       ~=? [ReservedID Case, Conid "Hello"]
+
+tests = test [test1, test2, test3]
+
+

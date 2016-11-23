839d71d985704fc3ff8b547117d618abb9307115
Curdled <joe.isaacs@live.co.uk>
2016-11-23 18:03:54 +0000

    clean up

--- JVHC/JVHC/src/CoreAST/Types.hs
+++ JVHC/JVHC/src/CoreAST/Types.hs
@@ -3,6 +3,11 @@ module CoreAST.Types
 
 import CoreAST.Kind
 import Infer.Id
+import Printing.PPrint
+import Printing.PPrintTypes
+
+import Printing.PPrint
+import Text.PrettyPrint
 
 type FreshId = Int
 
@@ -10,7 +15,7 @@ data Type = TVar  Tyvar
           | TCon  Tycon
           | TAp   Type    Type
           | TGen  FreshId
-          deriving (Show, Eq)
+          deriving Eq
 
 
 data Tyvar = Tyvar Id Kind
@@ -47,4 +52,28 @@ tUnit  = TCon $ Tycon "Unit" Star
 fn :: Type -> Type -> Type
 fn a b = TAp (TAp tArrow a) b
 
+
+instance PPrint Type where
+  pprint    = pptype 0
+  parPprint = pptype 10
+
+pptype d (TAp (TAp a x) y)
+    | a==tArrow    = ppParen (d>=5) (pptype 5 x <+> text "`fn`"
+                                                <+> pptype 0 y)
+pptype d (TAp l r) = ppParen (d>=10) (text "TAp" <+> pptype 10 l
+                                                 <+> pptype 10 r)
+pptype d (TGen n)  = ppParen (d>=10) (text "TGen" <+> int n)
+pptype d t
+    | t==tList     = text "tList"
+    | t==tArrow    = text "tArrow"
+    | t==tUnit     = text "tUnit"
+pptype d (TCon (Tycon i k))
+                   = text ('t':i)
+pptype d (TVar v)  = pprint v
+
+instance Show Type where
+  show = pretty
+
+instance PPrint Tyvar where
+  pprint (Tyvar v k)  = text v
 
